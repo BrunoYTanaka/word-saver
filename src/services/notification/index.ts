9f609b5a48b022bfbcecfb420e348bcacf911964
Bruno Y. Tanaka <bruno.tanaka@agiletv.com>
2026-01-07 11:05:58 -0400

    feat: simplify NotificationService by removing unused methods and types; streamline initialization and export

--- src/services/notification/index.ts
+++ src/services/notification/index.ts
@@ -1,6 +1,5 @@
 import { CoreNotificationService } from './core-notification-service'
 import { AlertScheduler } from './alert-scheduler'
-import type { NotificationOptions, PermissionStatus } from './types'
 import { FullAlert } from '../../types/alert'
 
 class NotificationService {
@@ -12,7 +11,6 @@ class NotificationService {
     this.scheduler = new AlertScheduler(this.core)
   }
 
-  // Initialize all services
   async init(): Promise<boolean> {
     const initialized = await this.core.init()
 
@@ -23,27 +21,6 @@ class NotificationService {
     return initialized
   }
 
-  // Delegate core methods
-  async requestPermission(): Promise<NotificationPermission> {
-    return await this.core.requestPermission()
-  }
-
-  async showNotification(
-    title: string,
-    options: NotificationOptions = {}
-  ): Promise<void> {
-    return await this.core.showNotification(title, options)
-  }
-
-  isEnabled(): boolean {
-    return this.core.isEnabled()
-  }
-
-  getPermissionStatus(): PermissionStatus {
-    return this.core.getPermissionStatus()
-  }
-
-  // Delegate scheduler methods
   async scheduleAlert(alert: FullAlert): Promise<boolean> {
     return await this.scheduler.scheduleAlert(alert)
   }
@@ -55,20 +32,6 @@ class NotificationService {
   async rescheduleAlerts(): Promise<void> {
     return await this.scheduler.rescheduleAlerts()
   }
-
-  // Delegate notification type methods
-  async showWordReviewReminder(
-    wordCount: number,
-    contexts: string
-  ): Promise<void> {
-    return await this.core.showWordReviewReminder(wordCount, contexts)
-  }
-
-  async showCongratsNotification(milestone: string): Promise<void> {
-    return await this.core.showCongratsNotification(milestone)
-  }
 }
 
-// Create singleton instance
-const notificationService = new NotificationService()
-export default notificationService
+export default new NotificationService()
